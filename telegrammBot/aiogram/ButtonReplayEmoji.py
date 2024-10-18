f363e9d59008bc1b46b184e379ea5ea2e1b071e1
AlexVigula <35034925+AlexVigula@users.noreply.github.com>
2024-10-18 20:33:24 +0300

    Create ButtonReplayEmoji.py

---
+++ telegrammBot/aiogram/ButtonReplayEmoji.py
@@ -0,0 +1,29 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import asyncio
+
+API_TOKEN = '5794892595:AAHR5bfnGgOZmwTGN1n9B-1SB7I60IP7DpU'  # Замените на ваш токен
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def send_welcome(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text='👤 Профиль'),
+            types.KeyboardButton(text="🚕 Меню"),
+            types.KeyboardButton(text='🚖 Создать заказ'),
+            types.KeyboardButton(text='💰 Баланс'),
+            types.KeyboardButton(text="💳 Пополнить"),
+            types.KeyboardButton(text='❓ Помощь'),
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)  # Добавлено resize_keyboard для удобства
+
+    await message.reply("Привет!\nЯ ТаксиБот от компании VIGULA !\nДавай я помогу тебе?", reply_markup=keyboard)
+
+if __name__ == '__main__':
+    # Запускаем бота 
+    asyncio.run(dp.start_polling(bot))
